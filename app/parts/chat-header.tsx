38953fa370ffce31f8b36a923b98ff0b8d159b38
dmeghana98 <43649126+dmeghana98@users.noreply.github.com>
2025-11-27 11:43:00 +0530

    Updated chat-header

--- app/parts/chat-header.tsx
+++ app/parts/chat-header.tsx
@@ -10,8 +10,8 @@ export function ChatHeaderBlock({ children, className }: { children?: React.Reac
 
 export function ChatHeader({ children }: { children: React.ReactNode }) {
     return (
-        <div className="w-full flex py-5 px-5 bg-linear-to-b from-background to-transparent">
-            {children}
+        <div className="w-full flex justify-start py-5 px-5 bg-linear-to-b from-background to-transparent">
+            <h1 className="font-semibold text-xl">Chat with MAHA</h1>
         </div>
     )
 }
