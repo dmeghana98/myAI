1709e62a0a9a766c47f3f8eb9fad6c2eba0a75a5
dmeghana98 <43649126+dmeghana98@users.noreply.github.com>
2025-11-27 16:54:08 +0530

    Update chat-header.tsx

--- app/parts/chat-header.tsx
+++ app/parts/chat-header.tsx
@@ -1,9 +1,17 @@
 import { cn } from "@/lib/utils";
 
-export function ChatHeader({ children }: { children: React.ReactNode }) {
+export function ChatHeaderBlock({ children, className }: { children?: React.ReactNode, className?: string }) {
     return (
-        <div className="w-full flex items-center py-4 px-6 bg-[#777BB0] min-h-[64px]">
+        <div className={cn("flex items-center gap-3", className)}>
             {children}
         </div>
-    );
+    )
+}
+
+export function ChatHeader({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="w-full flex items-center justify-between py-4 px-6 bg-[#787AB0] text-white font-bold">
+            {children}
+        </div>
+    )
 }
