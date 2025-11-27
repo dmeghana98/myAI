d4dac99a5ba16ea27a713d5419694007a9e36469
dmeghana98 <43649126+dmeghana98@users.noreply.github.com>
2025-11-27 20:20:54 +0530

    Update user-message.tsx

--- components/messages/user-message.tsx
+++ components/messages/user-message.tsx
@@ -5,7 +5,7 @@ import { MessageAvatar } from "../ai-elements/message";
 export function UserMessage({ message }: { message: UIMessage }) {
     return (
         <div className="whitespace-pre-wrap w-full flex justify-end items-center gap-2">
-            <div className="max-w-lg w-fit px-4 py-3 rounded-[20px] bg-[#777BB0] text-black">
+            <div className="max-w-lg w-fit px-4 py-3 rounded-[20px] bg-[#777BB0] text-white">
                 <div className="text-sm">
                     {message.parts.map((part, i) => {
                         switch (part.type) {
